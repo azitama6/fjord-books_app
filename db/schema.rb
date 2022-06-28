c39ca86ed7f3ed36bff504234ff7115a273843da
yusuke.nakazato <ajitama6@gmail.com>
2022-06-28 23:23:59 +0900

    user_id,post_code,nameを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_132828) do
+ActiveRecord::Schema.define(version: 2022_06_27_162258) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -32,8 +32,11 @@ ActiveRecord::Schema.define(version: 2022_06_20_132828) do
     t.string "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id", default: "", null: false
+    t.string "post_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
 end
