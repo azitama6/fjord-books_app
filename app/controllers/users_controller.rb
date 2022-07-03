3d715a37baac0ed931aa6bf10a42f91346ca9587
yusuke.nakazato <ajitama6@gmail.com>
2022-07-03 22:26:03 +0900

    アイコン表示用にストロングパラーメーターを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+
   def index
     @users = User.order(:id).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:icon)
   end
 end
