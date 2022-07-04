cd7fb5ddf7e6edb045c3dfa05715f1faeef9ca7b
yusuke.nakazato <ajitama6@gmail.com>
2022-07-05 02:57:36 +0900

    frozenstring修正時に誤って変更したclass名を修正

--- db/migrate/20220627084022_add_user_id_index_to_users.rb
+++ db/migrate/20220627084022_add_user_id_index_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddUserIdToUsers < ActiveRecord::Migration[6.1]
+class AddUserIdIndexToUsers < ActiveRecord::Migration[6.1]
   def change
     add_index :users, :user_id, unique: true
   end
