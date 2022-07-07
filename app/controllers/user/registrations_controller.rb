9776aea65d778add6dafdaa97fa884706c55c9d0
yusuke.nakazato <ajitama6@gmail.com>
2022-07-07 23:14:33 +0900

    パスワード無しで更新できないように修正

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -40,10 +40,6 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def update_resource(resource, params)
-    resource.update_without_current_password(params)
-  end
-
   def after_update_path_for(resource)
     user_path(resource)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,26 +6,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   before_create -> { self.user_id = SecureRandom.uuid }
-
-  def update_without_current_password(params, *options)
-    if params[:password].blank?
-      params.delete(:password)
-      params.delete(:password_confirmation) if params[:password_confirmation].blank?
-      params.delete(:current_password)
-      result = update(params, *options)
-    else
-      current_password = params.delete(:current_password)
-      result = if valid_password?(current_password)
-                 update(params, *options)
-               else
-                 assign_attributes(params, *options)
-                 valid?
-                 errors.add(:current_password, current_password.blank? ? :blank : :invalid)
-                 false
-               end
-    end
-
-    clean_up_passwords
-    result
-  end
 end
