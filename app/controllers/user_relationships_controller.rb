6feb2c16061e739c480fea2f65cf0ded9209dcc7
yusuke.nakazato <ajitama6@gmail.com>
2022-09-09 08:36:15 +0900

    ユーザフォロー時にユーザ詳細へロリダイレクトし、ノーティスの表示を行う変更

--- app/controllers/user_relationships_controller.rb
+++ app/controllers/user_relationships_controller.rb
@@ -3,11 +3,11 @@
 class UserRelationshipsController < ApplicationController
   def follow
     current_user.follow(params[:id])
-    redirect_to root_path
+    redirect_to user_url, notice: 'フォローしました'
   end
 
   def unfollow
     current_user.unfollow(params[:id])
-    redirect_to root_path
+    redirect_to user_url, notice: 'フォロー解除しました'
   end
 end
