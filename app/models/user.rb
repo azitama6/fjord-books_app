18ae303cc6b8da10dc70aeacf829cf8b9d16f3a7
yusuke.nakazato <ajitama6@gmail.com>
2022-09-25 18:20:17 +0900

    falseの返却の記述をifからガード節に変更、エラーキャッチ用にfind_by!記述変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,19 +12,15 @@ class User < ApplicationRecord
   has_many :followers, through: :follower_relationships, source: :user
 
   def follow(user_id)
-    if following?(User.find(user_id))
-      false
-    else
-      following_relationships.create(follow_id: user_id)
-    end
+    return false if following?(User.find(user_id))
+
+    following_relationships.create(follow_id: user_id)
   end
 
   def unfollow(user_id)
-    if following?(User.find(user_id))
-      following_relationships.find_by(follow_id: user_id).destroy
-    else
-      false
-    end
+    return false if following?(User.find(user_id))
+
+    following_relationships.find_by!(follow_id: user_id).destroy
   end
 
   def following?(user)
